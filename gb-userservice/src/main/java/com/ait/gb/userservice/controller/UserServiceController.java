68653b7f31f6039d6122ac0b080c056d3de1ceea
liuyijiang <liuyijiang3430@qq.com>
2013-10-21 23:30:20 +0800

    dwda

--- gb-userservice/src/main/java/com/ait/gb/userservice/controller/UserServiceController.java
+++ gb-userservice/src/main/java/com/ait/gb/userservice/controller/UserServiceController.java
@@ -2,16 +2,22 @@ package com.ait.gb.userservice.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.ait.gb.userservice.service.UserService;
 
 
 @Controller
 public class UserServiceController {
    
 	public static final Logger logger = LoggerFactory.getLogger(UserServiceController.class); 
+
+	@Autowired
+	private UserService userService;
 	
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public ModelAndView index(){
@@ -20,6 +26,7 @@ public class UserServiceController {
 		 logger.info("======333info");  
 		 logger.warn("======444warn");  
 		 logger.error("======555error");
+		 userService.test();
 		return new ModelAndView("index.jsp");
 	}
 	
---
+++ gb-userservice/src/main/java/com/ait/gb/userservice/service/UserService.java
@@ -0,0 +1,21 @@
+package com.ait.gb.userservice.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ait.mongodb.database.service.AitMongoDBTemplateService;
+
+@Service
+public class UserService {
+  
+	@Autowired
+	private AitMongoDBTemplateService service; 
+	
+	public void test(){
+//		UserEntity u = new UserEntity();
+//		u.setId("dsa");
+//		u.setName("dsad");
+		System.out.println(service);
+	}
+	
+}
